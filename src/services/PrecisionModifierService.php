774654fb41eb57a7700a0b525ae1009ec78adb48
Myke <michael.mwebaze@gmail.com>
2018-06-26 19:51:53 -0400

    using DI for certain services

--- src/services/PrecisionModifierService.php
+++ src/services/PrecisionModifierService.php
@@ -4,10 +4,13 @@ namespace Drupal\precision_modifier\services;
 
 use \Drupal\Core\Entity\EntityTypeManagerInterface;
 use \Drupal\Core\Database\Connection;
-use Drupal\field\Entity\FieldConfig;
+use Drupal\Core\Messenger\MessengerInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\field\Entity\FieldStorageConfig;
+use Drupal\Core\Config\ConfigFactoryInterface;
 
 class PrecisionModifierService implements PrecisionModifierServiceInterface {
+  use StringTranslationTrait;
   /**
    * @var \Drupal\Core\Entity\EntityTypeManagerInterface
    */
@@ -17,20 +20,39 @@ class PrecisionModifierService implements PrecisionModifierServiceInterface {
    * @var \Drupal\Core\Database\Connection
    */
   protected $connection;
-  public function __construct(Connection $connection, EntityTypeManagerInterface $entityTypeManager) {
+  /**
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * @var \Drupal\Core\Messenger\MessengerInterface $messenger
+   */
+  protected $messenger;
+
+  /**
+   * PrecisionModifierService constructor.
+   *
+   * @param \Drupal\Core\Database\Connection $connection
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $configFactory
+   * @param \Drupal\Core\Messenger\MessengerInterface $messenger
+   */
+  public function __construct(Connection $connection, EntityTypeManagerInterface $entityTypeManager,
+                              ConfigFactoryInterface $configFactory, MessengerInterface $messenger) {
     $this->connection = $connection;
     $this->entityTypeManager = $entityTypeManager;
+    $this->configFactory = $configFactory;
+    $this->messenger = $messenger;
   }
-  public function increasePrecision($field, $bundle, $precision) {
+  public function increasePrecision($field, $bundle, $precision, $scale) {
     $database = $this->connection;
     $tables = [
       "node_revision__{$field}",
       "node__{$field}",
     ];
-    $settings = ['precision' => $precision, 'scale' => 2,];
+    $settings = ['precision' => $precision, 'scale' => $scale,];
     $existingData = [];
-
-
 
     foreach ($tables as $table) {
       $existingData[$table] = $database->select($table)
@@ -41,7 +63,7 @@ class PrecisionModifierService implements PrecisionModifierServiceInterface {
       $database->truncate($table)->execute();
     }
 
-    $config = \Drupal::service('config.factory')->getEditable('field.storage.node'.$field);
+    $config = $this->configFactory->getEditable('field.storage.node.'.$field);
     $config->set('settings', $settings)->save();
     $fieldStorage = FieldStorageConfig::loadByName('node', $field);
     $fieldStorage->set('settings', $settings);
@@ -58,5 +80,12 @@ class PrecisionModifierService implements PrecisionModifierServiceInterface {
       }
       $insert_query->execute();
     }
+    $this->entityTypeManager->getStorage('entity_form_display')
+      ->load('node' . '.' . $bundle . '.' . 'default')
+      ->setComponent($field, ['region' => 'content'])->save();
+    $this->entityTypeManager->getStorage('entity_view_display')
+      ->load('node' . '.' . $bundle . '.' . 'default')
+      ->setComponent($field, ['region' => 'content'])->save();
+    $this->messenger->addMessage($this->t('Successfully increased precision or scale'));
   }
 }
--- src/services/PrecisionModifierServiceInterface.php
+++ src/services/PrecisionModifierServiceInterface.php
@@ -4,5 +4,5 @@ namespace Drupal\precision_modifier\services;
 
 
 interface PrecisionModifierServiceInterface {
-  public function increasePrecision($field, $bundle, $precision);
+  public function increasePrecision($field, $bundle, $precision, $scale);
 }
